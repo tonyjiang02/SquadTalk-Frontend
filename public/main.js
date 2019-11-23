24496025a0863986c275c780ac5b874dca502ac2
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-11-23 14:43:51 -0800

    sklvmsd

--- public/main.js
+++ public/main.js
@@ -1,7 +1,6 @@
 $("#submitMessage").click(() => {
-    var id = $("#idInput").val()
-    var message = $("#messageInput").val()
-    sendMessage(message, id)
-})
-
+  var id = $("#idInput").val();
+  var message = $("#messageInput").val();
+  sendMessage(message, id);
+});
 
