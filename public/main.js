13c7a6d183af9c52b200d97dadb4db60a9876cc7
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-11-23 18:33:43 -0800

    Oof

--- public/main.js
+++ public/main.js
@@ -2,3 +2,32 @@ $("#login-button").click(e => {
   e.preventDefault();
   loginWithGoogle();
 });
+
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then(result => {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          token = result.credential.accessToken;
+          console.log(result);
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+      })
+      .catch(error => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  }
+});
