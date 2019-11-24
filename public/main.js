760cd6b6d7c67b0cc9a6e24cad59148fb877d5a4
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-11-23 16:06:19 -0800

    kjnsdkjv

--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
-$("#submitMessage").click(() => {
-  preventDefault();
+$("#submitMessage").click(e => {
+  e.preventDefault();
   var id = $("#idInput").val();
   var message = $("#messageInput").val();
   sendMessage(message, id);
 });
-$("#login-button").click(() => {
-  preventDefault();
+$("#login-button").click(e => {
+  e.preventDefault();
   loginWithGoogle();
 });
