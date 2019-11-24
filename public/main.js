67c648d563f29b9aa6d4ed334afe4b17e97fcdaa
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-11-24 04:58:26 -0800

    sjdvnsjdnsksjdvnd

--- public/main.js
+++ public/main.js
@@ -14,30 +14,37 @@ firebase.auth().onAuthStateChanged(user => {
       e.preventDefault();
       window.location.replace("profile.html");
     });
-    firebase
-      .auth()
-      .getRedirectResult()
-      .then(result => {
-        if (result.credential && result !== null) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          token = result.credential.accessToken;
-          createUser(result.user.email);
-          console.log(result);
-          // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
-        console.log(user);
-      })
-      .catch(error => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
   }
 });
+
+firebase
+  .auth()
+  .getRedirectResult()
+  .then(result => {
+    if (result.credential && result !== null) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      token = result.credential.accessToken;
+      console.log(result.additionalUserInfo.isNewUser);
+      // createUser(result.user.email);
+      if (result.additionalUserInfo.isNewUser) {
+        window.location.replace("settings.html");
+      } else {
+        window.location.replace("profile.html");
+      }
+      console.log(result);
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user);
+  })
+  .catch(error => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
